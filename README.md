690765f6c64c29d802dcd2e8f7507bd5c9cea907
Nikhil Chatterjee <nikhilc1527@gmail.com>
2023-02-07 17:23:33 -0600

    finished

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
-# perception
+# Perception Coding Challenge
 
-This is the perception project.
+## My solution
 
-# Building and installing
+![](https://github.com/nikhilc1527/Perception-Coding-Challenge-WA/blob/main/static/my_output.png)
 
-See the [BUILDING](BUILDING.md) document.
+## Methodology
 
-# Contributing
+Firstly, the cones needed to be detected and all other noise needed to be removed. Since the cones are bright orange, I first slightly blurred the image and then filtered the image based on a very high saturation value. This gave me a black and white image with the silhouettes of the cones as white. Then, I used the simpleblobdetector to detect the positions of each of the cones in the image, and got a list of points from that. To distinguish the left side from the right side, I found the position in the list of points where the slope changed, and used that to separate the points. After that, I had a list of points for the left and right sides, and ran the least squares algorithm on each of them to get the final lines.
 
-See the [CONTRIBUTING](CONTRIBUTING.md) document.
+# What did I try and what did not work
 
-# Licensing
+I tried detecting the cones using a triangular kernel passed over the image. This did not provide nearly good enough results, because the size, orientation, and shape of each of the cones was different, and the size and shape of the exit sign was not different enough. I realized that I do not have to filter just by the triangular shape of the cones, but rather just the bright color that they have.
 
-<!--
-Please go to https://choosealicense.com/licenses/ and choose a license that
-fits your needs. The recommended license for a project of this type is the
-GNU AGPLv3.
--->
+# What libraries are used
+
+I used just the opencv library, version 4.7.0
--- source/main.cpp
+++ source/main.cpp
@@ -7,12 +7,9 @@
 
 using namespace cv;
 
-void print_arr(const auto &arr) {
-  for (auto i : arr) {
-    std::cout << i.first << " " << i.second << '\n';
-  }
-}
-
+// least squares formula for attaining the getting of best fit
+// param: vector of points where each pair is {x, y}
+// return: {b, m} in the line y=mx+b
 std::pair<double, double> least_squares(std::vector<std::pair<double, double>> points) {
   // using formula from https://www.varsitytutors.com/hotmath/hotmath_help/topics/line-of-best-fit
   double mean_x = 0;
@@ -34,9 +31,10 @@ std::pair<double, double> least_squares(std::vector<std::pair<double, double>> p
   return {b, m};
 }
 
+// utility to show image
+// needed named window of normal size because otherwise the image is too big for my screen
 void show_img(const auto &img, const std::string& window_name) {
   namedWindow(window_name, WINDOW_NORMAL);
-  // cvtColor(img, img, COLOR_HSV2BGR);
   imshow(window_name, img);
 }
 
@@ -45,19 +43,23 @@ auto main() -> int {
   using Img = decltype(red_img);
   cvtColor(red_img, red_img, COLOR_BGR2HSV);
 
+  // use median blur
   Img blurred;
   medianBlur(red_img, blurred, 19);
 
+  // color filter
   auto lower = Mat({0, 200, 150});
   auto higher = Mat({200, 255, 255});
   Img filtered;
   inRange(blurred, lower, higher, filtered);
 
+  // instantiate blob detector
   auto params = SimpleBlobDetector::Params();
   params.filterByColor = true;
   params.blobColor = 255;
   auto blob_detector = SimpleBlobDetector::create(params);
 
+  // use blob detector
   Img detected;
   std::vector<KeyPoint> keypoints;
   blob_detector->detect(filtered, keypoints);
@@ -68,13 +70,17 @@ auto main() -> int {
   show_img(filtered, "filtered");
   show_img(detected, "detected");
 
+  // gather the keypoints
   std::vector<std::pair<double, double>> points;
   for (auto i : keypoints) points.emplace_back(i.pt.x, i.pt.y);
 
   std::sort(points.begin(), points.end());
 
-  print_arr(points);
+  for (auto i : points) {
+    std::cout << i.first << ", " << i.second << '\n';
+  }
 
+  // find the ratios of consecutive slopes of the points
   std::vector<std::pair<double, int>> ratios;
   for (int i = 1; i < points.size() - 1; ++i) {
     auto slope1 = (points[i].second - points[i-1].second) / (points[i].first - points[i-1].first);
@@ -83,6 +89,7 @@ auto main() -> int {
   }
   for (auto i : ratios) std::cout << i.first << "\n";
 
+  // after sorting, the one with the least ratio is rightmost of left side, and one with most ratio is leftmost of right side
   std::sort(ratios.begin(), ratios.end());
 
   std::cout << ratios[0].first << "\n";
@@ -90,6 +97,7 @@ auto main() -> int {
   std::cout << ratios[ratios.size() - 1].first << "\n";
   std::cout << ratios[ratios.size() - 1].second << "\n";
 
+  // separate left and right sides
   std::vector<std::pair<double, double>> left_side, right_side;
   for (int i = 0; i <= ratios[0].second; i++) {
     left_side.push_back(points[i]);
@@ -107,6 +115,7 @@ auto main() -> int {
     std::cout << i.first << ", " << i.second << '\n';
   }
 
+  // draw line through each point to make sure that I have the correct left and right sides
   // for (int i = 1; i < left_side.size(); i++) {
   //   line(final_img, Point(left_side[i-1].first, left_side[i-1].second), Point(left_side[i].first, left_side[i].second), Scalar(0, 255, 0), 5);    
   // }
@@ -114,6 +123,7 @@ auto main() -> int {
   //   line(final_img, Point(right_side[i-1].first, right_side[i-1].second), Point(right_side[i].first, right_side[i].second), Scalar(0, 255, 0), 5);    
   // }
 
+  // perform least squares regression and output image
   auto final_img = imread("static/red.png", IMREAD_COLOR);
   
   auto [left_b, left_m] = least_squares(left_side);
